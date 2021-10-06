b2e7c3a8ca3658ffa7ef24cd4d474f64da8e97fc
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-06 12:22:41 +0530

    Add project files.

---
+++ Part21InheritanceBasics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Part21InheritanceBasics
+{
+    class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("ParentClass Contructore Called"); ;
+        }
+        public ParentClass(string s)
+        {
+            Console.WriteLine("String is={0} ", s);
+
+        }
+    }
+    class Child :ParentClass
+    {
+        public Child():base("Shubham")
+        {
+            Console.WriteLine("Child Class Constructor Called");
+
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        
+            Child c = new Child();
+            
+        }
+    }
+}
